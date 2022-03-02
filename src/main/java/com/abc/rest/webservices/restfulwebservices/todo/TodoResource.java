5143bc8b6098f43b80f63ccd5d094020f4d67ea9
Ram Pandey <ram.pandey22@outlook.com>
2022-03-02 23:32:18 +1100

    changed cors for todcontroller

--- src/main/java/com/abc/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/abc/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 public class TodoResource {
 
