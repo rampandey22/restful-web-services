ffb1c36e99783baf56a519cd7febeb1c641ba172
Ram Pandey <ram.pandey22@outlook.com>
2022-03-03 02:40:52 +1100

    removed user

--- src/main/java/com/abc/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/abc/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -15,8 +15,7 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
   static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
   static {
-    inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
-        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+   
     inMemoryUserList.add(new JwtUserDetails(1L, "rampandey",
             "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_3"));
     inMemoryUserList.add(new JwtUserDetails(1L, "abc",
