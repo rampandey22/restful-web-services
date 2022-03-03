2c0a8aaeb710f2cf77e421d13f85879ad490e90a
Ram Pandey <ram.pandey22@outlook.com>
2022-03-03 12:44:16 +1100

    changed tod list

--- src/main/java/com/abc/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ src/main/java/com/abc/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -13,9 +13,9 @@ public class TodoHardcodedService {
 	private static long idCounter = 0;
 
 	static {
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn to Dance 2", new Date(), false));
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn about Microservices 2", new Date(), false));
-		todos.add(new Todo(++idCounter, "in28minutes", "Learn about Angular", new Date(), false));
+		todos.add(new Todo(++idCounter, "abc", "Learn to Dance 2", new Date(), false));
+		todos.add(new Todo(++idCounter, "abc", "Learn about Microservices 2", new Date(), false));
+		todos.add(new Todo(++idCounter, "abc", "Learn about Angular", new Date(), false));
 	}
 
 	public List<Todo> findAll() {
